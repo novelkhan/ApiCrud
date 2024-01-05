72a877bec436eb64926173f30810fe4a98b1d1fb
novelkhan <novel4004@gmail.com>
2024-01-06 04:45:58 +0600

    Navbar & Footer code is added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  collapsed = true;
+
+  constructor(public accountService: AccountService) { }
+
+  logout() {
+    //this.accountService.logout();
+  }
+
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+  }
 }
