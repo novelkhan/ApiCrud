702ee226853de54d60be44f643e772697c3c5eeb
novelkhan <novel4004@gmail.com>
2024-01-07 02:56:50 +0600

    Man model is added

---
+++ src/app/man/models/man.model.ts
@@ -0,0 +1,13 @@
+export interface Man {
+    Id: number,
+    Name: string,
+    Age: number,
+    City: string,
+    Country: string
+}
+
+/* public int Id { get; set; }
+public string Name { get; set; }
+public int Age { get; set; }
+public string City { get; set; }
+public string Country { get; set; } */
